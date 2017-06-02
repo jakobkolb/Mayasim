84c310bfe8432eaf6db5ab521b36d85a9b2114a7
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2017-06-02 12:16:34 +0200

    retreived lost evaluation notebooks. Will most likely lead to merge conflicts

--- tests/test_X2.py
+++ tests/test_X2.py
@@ -1,5 +1,5 @@
 """this is the test file for X7, the experiment concerning income from agriculture and ecosystem services"""
 
-from ..Experiments import mayasim_X7_scan_r_es_and_r_agg as X7
+from Experiments import mayasim_X2_scan_r_es_and_r_agg as X7
 
 assert X7.run_experiment(['testing', 1]) == 1
