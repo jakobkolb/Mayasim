d864b35ed96783744a7b8a2ac09b386ea9e863bd
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2017-07-20 10:33:46 +0200

    added submit script for X8

---
+++ cluster_tools/Submit_x8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --qos=short
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=Maya_8
+#SBATCH --output=ms_x8_%j.out
+#SBATCH --error=ms_x8_%j.err
+#SBATCH --account=copan
+#SBATCH --nodes=10
+#SBATCH --tasks-per-node=16
+
+module load compiler/intel/16.0.0
+module load hpc/2015 anaconda/2.3.0
+export I_MPI_PMI_LIBRARY=/p/system/slurm/lib/libpmi.so
+export OMP_NUM_THREADS=1
+
+##################
+echo "------------------------------------------------------------"
+echo "SLURM JOB ID: $SLURM_JOBID"
+echo "$SLURM_NTASKS tasks"
+echo "------------------------------------------------------------"
+
+cd ../Experiments/
+srun -n $SLURM_NTASKS python mayasim_X8_long_term_dynamics.py 0
