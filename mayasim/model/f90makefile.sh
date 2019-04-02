b823064fa824cd4b2328edc829dd179d27223ef6
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2019-04-02 16:35:54 +0200

    removed openmp compilation flag for f90 routines as it was causing segfaults on the cluster

--- mayasim/model/f90makefile.sh
+++ mayasim/model/f90makefile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-f2py    --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
-f2py3   --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
-f2py3.5 --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
+module load compiler/intel/16.0.0
+module load hpc/2015 anaconda/2.3.0
+export I_MPI_PMI_LIBRARY=/p/system/slurm/lib/libpmi.so
+export OMP_NUM_THREADS=1
+
+f2py    --f90flags='-O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
+f2py3   --f90flags='-O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
+f2py3.5 --f90flags='-O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
 
