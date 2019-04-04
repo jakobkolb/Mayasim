4931d0995f8565b556a1cf7cf63fa27f936d0a40
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2019-04-04 11:25:31 +0200

    fixed a bug in array indexing that should have bitten me a long time ago but didn't.

--- mayasim/model/f90makefile.sh
+++ mayasim/model/f90makefile.sh
@@ -4,3 +4,8 @@ f2py    --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m
 f2py3   --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
 f2py3.5 --f90flags='-fopenmp -O3 -ftree-vectorize' -lgomp -c  f90routines.f90 -m f90routines
 
+
+#f2py    --f90flags='-g -fbounds-check' -lgomp -c  f90routines.f90 -m f90routines
+#f2py3   --f90flags='-g -fbounds-check' -lgomp -c  f90routines.f90 -m f90routines
+#f2py3.5 --f90flags='-g -fbounds-check' -lgomp -c  f90routines.f90 -m f90routines
+
